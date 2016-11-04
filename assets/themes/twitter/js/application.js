a14fc0707a16c616d8738e99f368025bea1e6ada
Andrew Newhouse <andrewwn@gmail.com>
2016-11-03 21:03:03 -0400

    updates to collaborative interest form

--- assets/themes/twitter/js/application.js
+++ assets/themes/twitter/js/application.js
@@ -1,4 +1,32 @@
 
 $(function () {
 	$('a').tooltip();
+
+// Capture Collaborative Interest Form Submit
+  $("#ciForm").submit(function(){
+    $.ajax({
+      type: 'POST',
+      url: "https://docs.google.com/forms/d/e/1FAIpQLSc-vwliaOCRh-Ag5aq_6E_xEgsKeNnO-58kIoYwksR4z8q4Qw/formResponse",
+      data: $('#ciForm').serialize(), 
+      complete: function() {
+      	window.location.href="/thank-you/";
+      }
+    });
+
+    // prevent submitting form again
+    return false;
+  });
+
+// Display/Hide ember or Provider form fields depending on selection
+  $("input[name$='entry.1166974658']").on( "change", function() {
+    var rdVal = $(this).val();
+    if (rdVal == "Member") {
+    	$("div#provider-type-field").hide();
+    	$("div#member-skills-field").show();
+    } else {
+    	$("div#member-skills-field").hide();
+    	$("div#provider-type-field").show();
+    }
+  }); 
+
 });
---
+++ thank-you.md
@@ -0,0 +1,30 @@
+---
+layout: homepage
+title: Collaborative Workspace Interest Form
+tagline: Collaborative Workspace Interest Form
+sections:
+ intro: Collaborative Workspace
+ brief: Interest Form
+ icon: fa fa-users fa-5x
+---
+
+<div style="background: #344555; color: #fff;">
+<div class="container">
+	<div class="row" style="margin-top: 50px; margin-bottom: 50px;">
+		<div class="col-md-3 text-center">
+			<h1><i class="{{ page.sections['icon'] }}"></i></h1>
+		</div>
+		<div class="col-md-9">
+			<h1><b>{{ page.sections['intro'] }}</b></h1>
+			<h3>{{ page.sections['brief'] }}</h3>
+		</div>
+	</div>
+</div>
+</div>
+
+<section class="content-section">
+	<div class="container">
+		<h2 class="section-heading">{{ page.form_title }}</h2>
+    <div class="alert alert-success" role="alert"> <strong>Thank you!</strong>  We'll be in touch.</div>
+	</div>
+</section>
