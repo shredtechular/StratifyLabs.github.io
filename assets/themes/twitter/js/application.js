a5ba56e70304ca584dcf3d3bedbdaa6543e76b8e
Andrew Newhouse <andrewwn@gmail.com>
2016-11-28 15:29:08 -0500

    updating blog index page list of posts & autoscrolling

--- assets/themes/twitter/js/application.js
+++ assets/themes/twitter/js/application.js
@@ -197,3 +197,101 @@ function replaceValidationUI( form ) {
         }
     });
 }
+
+$(function() {
+
+  var postURLs,
+      isFetchingPosts = false,
+      shouldFetchPosts = true,
+      postsToLoad = $(".container article").size(),
+      loadNewPostsThreshold = 100,
+      catID = $("#catID").attr("data-catID");
+
+  // Load the JSON file containing all URLs
+  $.getJSON('/all-posts.json', function(data) {
+    postURLs = data["posts"];
+
+    // If there aren't any more posts available to load than already visible, disable fetching
+    if (postURLs.length <= postsToLoad)
+      disableFetching();
+    else {
+      // Filter array of posts by category if on category page
+      if (catID) {
+        for (var ii=0; ii < postURLs.length; ii++) {
+          if (postURLs[ii].categories.indexOf(catID) == -1 ) {
+            postURLs.splice(ii,1);
+            ii--;
+          }
+        }
+        if (postURLs.length <= postsToLoad)
+          disableFetching();
+      }
+    }
+  });
+
+  // If there's no spinner, it's not a page where posts should be fetched
+  if ($(".infinite-spinner").length < 1)
+    shouldFetchPosts = false;
+
+  // Are we close to the end of the page? If we are, load more posts
+  $(window).scroll(function(e){
+    if (!shouldFetchPosts || isFetchingPosts) return;
+
+    var windowHeight = $(window).height(),
+        windowScrollPosition = $(window).scrollTop(),
+        bottomScrollPosition = windowHeight + windowScrollPosition,
+        documentHeight = $(document).height();
+
+    // If we've scrolled past the loadNewPostsThreshold, fetch posts
+    if ((documentHeight - loadNewPostsThreshold) < bottomScrollPosition) {
+      fetchPosts();
+    }
+  });
+
+  // Fetch a chunk of posts
+  function fetchPosts() {
+    // Exit if postURLs haven't been loaded
+    if (!postURLs) return;
+
+    isFetchingPosts = true;
+
+    // Load as many posts as there were present on the page when it loaded
+    // After successfully loading a post, load the next one
+    var loadedPosts = 0,
+        postCount = $(".posts-list article").size(),
+        callback = function() {
+          loadedPosts++;
+          var postIndex = postCount + loadedPosts;
+
+          if (postIndex > postURLs.length-1) {
+            disableFetching();
+            return;
+          }
+
+          if (loadedPosts < postsToLoad) {
+            fetchPostWithIndex(postIndex, callback);
+          } else {
+            isFetchingPosts = false;
+          }
+        };
+
+    fetchPostWithIndex(postCount + loadedPosts, callback);
+  }
+
+  function fetchPostWithIndex(index, callback) {
+    var postURL = postURLs[index].url;
+
+    $.get(postURL, function(data) {
+      $(data).find(".loading-more").appendTo(".posts-list").removeClass("hidden");
+      callback();
+    });
+  }
+
+  function disableFetching() {
+    shouldFetchPosts = false;
+    isFetchingPosts = false;
+    $(".infinite-spinner").hide();
+  }
+
+});
+
--- tips.md
+++ tips.md
@@ -4,16 +4,41 @@ title : Embedded Design Tips
 category: embedded
 header : Post Archive
 group: navigation
+sections:
+ intro: Blog
+ brief: Embedded Design Tips
+ icon: fa fa-newspaper-o fa-5x
 ---
 {% include JB/setup %}
 
 
-<div>
-{% for post in site.posts limit: 100 %}
-	<div class='tagged {{ post.tags | join:" " }}'>
-	<h2><a href="{{ post.url }}">{{ post.title }}</a> <small>{{ post.date | date:"%Y-%m-%d" }}</small></h2>
-	<p>{{ post.excerpt }}</p>
-	<hr>
-	</div>
+{% for post in site.posts limit: 5 %}
+
+<article class="post-card">
+			<a href="{{ post.url }}" class="post-title"><h2>{{ post.title }}</h2> </a>
+			<div class="post-categories">
+				<strong>Categories:</strong>
+					{% for cat in post.category %}
+					<a href="https://jevelin.shufflehound.com/category/design/" style="font-size:14px;text-transform:uppercase;">{{ cat }}</a>{% unless forloop.last %},{% endunless %}
+					{% endfor %}
+			</div>
+			<div class="post-date">
+				<strong>{{ post.date | date:"%B %-d, %Y" }}</strong>
+			</div>
+			<div class="clearfix"></div>
+			<div class="post-content">
+				{{ post.excerpt }}
+				<p><a class="btn btn-info" href="{{ post.url }}" title="Read More">Read More</a></p>
+			</div>
+			<div class="post-meta">
+				<div class="sh-columns post-meta-comments">
+					<span class="post-meta-categories"> <i class="fa fa-tag"></i>
+						{% for tag in post.tags %}
+						<a href="/">{{ tag }}</a>
+						{% endfor %}
+					</span>
+				</div>
+			</div>
+</article>
+
 {% endfor %}
-</div>
